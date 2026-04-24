fe708022d8973068888d42c60a8c66aa39e5a605
Marcos Lucas de Souza Machado <118454658+F1reZap@users.noreply.github.com>
2026-04-24 16:12:40 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,3 +1,23 @@
-<script>
-    console.log("Este é um script interno!");
-</script>
+// aparecer e sumir a barra lateral
+document.getElementById('toggleSidebar').addEventListener('click', function() {
+    var sidebar = document.querySelector('.Side_Bar');
+    sidebar.style.display = sidebar.style.display === 'none' ? 'flex' : 'none';
+});
+
+document.getElementById('meuCheckbox').addEventListener('change', function() {
+    const spanData = document.getElementById('dataRegistro');
+    
+    if (this.checked) {
+        //aqui que cria o objeto de data
+        const agora = new Date();
+        
+        //Formata a data e hora para o padrão BR
+        const dataFormatada = agora.toLocaleDateString('pt-BR');
+        const horaFormatada = agora.toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' });
+        
+        spanData.textContent = `Clicado em: ${dataFormatada} às ${horaFormatada}`;
+    } else {
+        //Limpa o texto
+        spanData.textContent = '';
+    }
+});
